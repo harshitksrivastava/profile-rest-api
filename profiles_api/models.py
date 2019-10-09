fc936b577b294349aee192e21d40c156ab263443
harshitksrivastava <harsh0311@gmail.com>
2019-10-10 03:45:10 +0530

    Added custom user profile model,manager and migrations

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -1,3 +1,56 @@
 from django.db import models
-
+# the below are to be imported to change the django user model
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
 # Create your models here.
+
+
+class UserProfileManager(BaseUserManager):
+    """Manager for user profiles"""
+
+    def create_user(self,email,name,password=None):
+        """Create anew user profile"""
+        if not email:
+            raise ValueError('Users must have an email address')
+    # normalizing(converting to lower case ) second half of email
+        email = self.normalize_email(email)
+        user = self.model(email=email,name=name)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self,email,name,password):
+        """Create and save a new superuser with details"""
+        user = self.create_user(email,name=name,password=password)
+        user.is_superuser=True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
+
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """ Database model for users in the system"""
+    email = models.EmailField(max_length=255,unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+
+    objects = UserProfileManager()
+# as we are over-riding default username field with email ...So,
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['name']
+
+
+    def get_full_name(self):
+        """retrieve full name of the user"""
+        return self.name
+
+    def get_short_name(self):
+        """retrieve short name of the user"""
+        return self.name
+
+    def __str__(self):
+        """return string representation of the user"""
+        return self.email
